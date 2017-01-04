6894610893ea0cde7fce40aea1896be9a9c15058
Tom Barrett <tbtommyb@gmail.com>
2017-01-04 19:08:57 +0000

    Separated out home view content

---
+++ src/assets/home.js
@@ -0,0 +1,8 @@
+export default {
+  headline: ['Tom', 'Barrett'],
+  summaryPara: 'Tom Barrett is a wonderful front-end developer. Tom Barrett is a wonderful front-end developer.',
+  paragraphs: [
+    'Tom is going to write lots of interesting things about web development and why you should give him a job.',
+    'Tom is going to write lots of interesting things about web development and why you should give him a job.'
+  ]
+};
