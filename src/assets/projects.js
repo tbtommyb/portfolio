a2f424a8274546e87b56bb1905ebaf742aa5ac13
Tom Barrett <tbtommyb@gmail.com>
2017-01-04 18:18:11 +0000

    Extract portfolio data to asset file and refactor

---
+++ src/assets/projects.js
@@ -0,0 +1,43 @@
+export default 
+[
+  {
+    title: 'Concertspot',
+    projectSrc: 'https://www.concertspot.co.uk',
+    imgSrc: require('../assets/images/concertspot-narrow.jpg'),
+    firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
+    paragraphs: [
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.',
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.'
+    ]
+  },
+  {
+    title: 'OxGrow',
+    projectSrc: 'https://www.oxgrow.org',
+    imgSrc: require('../assets/images/oxgrow-narrow.jpg'),
+    firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
+    paragraphs: [
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.',
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.'
+    ]
+  },
+  {
+    title: 'Wedding',
+    projectSrc: 'https://www.tomandgeorgie.co.uk',
+    imgSrc: require('../assets/images/wedding-narrow.jpg'),
+    firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
+    paragraphs: [
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.',
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.'
+    ]
+  },
+  {
+    title: 'Number one app',
+    projectSrc: 'https://www.number-oneapp.herokuapp.com',
+    imgSrc: require('../assets/images/number-one-narrow.jpg'),
+    firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
+    paragraphs: [
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.',
+      'Tom is going to write lots of interesting things about web development and why you should give him a job.'
+    ]
+  }
+];
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <sidebar :imgSrc="currentProject.imgSrc"></sidebar>
+    <sidebar
+      :imgSrc="currentProject.imgSrc"></sidebar>
     <div class="home content">
+      <direction-buttons :next="next" :previous="previous"></direction-buttons>
       <header>
         <h1>Portfolio</h1>
-        <h2>{{ currentProject.title }}</h2>
+        <a class="heading-link" :href="currentProject.projectSrc">
+          <h2>{{ currentProject.title }}</h2>
+        </a>
       </header>
-      <direction-buttons :next="next" :previous="previous"></direction-buttons>
       <div class="content-col">
         <strong>{{ currentProject.firstPara }}</strong>
         <p v-for="para in currentProject.paragraphs">{{ para }}</p>
@@ -18,6 +21,7 @@
 <script>
 import Sidebar from './Sidebar';
 import DirectionButtons from './DirectionButtons';
+import projects from '../assets/projects';
 
 export default {
   name: 'portfolio',
@@ -26,44 +30,16 @@ export default {
       return this.projects[this.$route.params.id]
     },
     next() {
-      return (this.$route.params.id + 1)  % this.projects.length;
+      return (parseInt(this.$route.params.id, 10) + 1)  % this.projects.length;
     },
     previous() {
-      const previous = this.$route.params.id - 1;
+      const previous = parseInt(this.$route.params.id, 10) - 1;
       return previous < 0 ? this.projects.length - 1 : previous; 
     }
   },
   data () {
     return {
-      projects: [
-        {
-          title: 'Concertspot',
-          imgSrc: require('../assets/images/concertspot-narrow.jpg'),
-          firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
-          paragraphs: [
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.',
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.'
-          ]
-        },
-        {
-          title: 'OxGrow',
-          imgSrc: require('../assets/images/oxgrow-narrow.jpg'),
-          firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
-          paragraphs: [
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.',
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.'
-          ]
-        },
-        {
-          title: 'Wedding',
-          imgSrc: require('../assets/images/number-one-narrow.jpg'),
-          firstPara: 'Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. Lots of info about the project. ',
-          paragraphs: [
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.',
-            'Tom is going to write lots of interesting things about web development and why you should give him a job.'
-          ]
-        }
-      ]
+      projects
     };
   },
   components: {
@@ -76,9 +52,22 @@ export default {
 
 <style scoped>
 
+header {
+  overflow-x: hidden;
+}
+
+.heading-link {
+  text-decoration: none;
+  color: red;
+}
+
+.heading-link h2 {
+  font-size: 4em;
+}
+
 .content {
   margin: 0 5%;
-  margin-top: 6%;
+  margin-top: 12%;
 }
 
 .home.content {
@@ -88,4 +77,25 @@ export default {
 .content-col {
   margin: 5% 2% 0 24%;
 }
+
+@media (max-width: 600px) {
+  .heading-link h1 {
+    font-size: 4em;
+  }
+
+  .heading-link h2 {
+    font-size: 2.5em;
+  }
+}
+
+@media (max-width: 500px) {
+  .heading-link h2 {
+    font-size: 2em;
+  }
+
+  .content-col {
+    margin: 5% 2%;
+  }
+}
+
 </style>
