323928155911334514ed1047aa055cd476325528
Tom Barrett <tbtommyb@gmail.com>
2017-01-05 18:42:14 +0000

    Updated home page text

--- src/assets/home.js
+++ src/assets/home.js
@@ -2,7 +2,7 @@ export default {
   headline: ['Tom', 'Barrett'],
   summary: 'I am a full-stack web developer seeking work in London. I like writing websites and web apps with sensible code and clean UIs. I enjoy learning new technologies and expanding my skills.',
   paragraphs: [
-    'I am a graduate of Founders & Coders and am comfortable across the full web stack. Most of my experience is in JavaScript but I\'m very happy to learn new language stacks.',
-    'I aspire to be a great coder and spend probably slightly too much of my free time learning about computers, software development and playing (currently primarily audio programming and Arduino).'
+    'Please take a look at my portfolio to see my experience across the full web stack. I\'m most comfortable with JavaScript but I\'m very happy to learn new language stacks.',
+    'I aspire to be a great coder and currently spend probably slightly too much of my free time learning about computers, software development and playing (at the moment primarily audio programming and Arduino).'
   ]
 };
