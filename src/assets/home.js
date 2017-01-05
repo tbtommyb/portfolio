1be3231c037b355d97a80856826a368a9f701539
Tom Barrett <tbtommyb@gmail.com>
2017-01-05 10:23:26 +0000

    Add full stops

--- src/assets/home.js
+++ src/assets/home.js
@@ -2,7 +2,7 @@ export default {
   headline: ['Tom', 'Barrett'],
   summary: 'I am a full-stack web developer seeking work in London. I like writing websites and web apps with sensible code and clean UIs. I enjoy learning new technologies and expanding my skills.',
   paragraphs: [
-    'I am a graduate of Founders & Coders and am comfortable across the full web stack. Most of my experience is in JavaScript but I\'m very happy to learn new language stacks',
+    'I am a graduate of Founders & Coders and am comfortable across the full web stack. Most of my experience is in JavaScript but I\'m very happy to learn new language stacks.',
     'I aspire to be a great coder and spend probably slightly too much of my free time learning about computers, software development and playing (currently primarily audio programming and Arduino).',
     'Please feel free to get in touch with me.'
   ]
